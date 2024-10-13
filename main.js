c1f4fc3e86d02272e87ce5b94f7cc19313a64126
Ikchha <102365567+Ikchha@users.noreply.github.com>
2024-10-13 13:20:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 status = "";
+objects = [];
 
 function setup(){
     canvas = createCanvas(380, 380);
@@ -18,4 +19,25 @@ function gotResults(){
     }
     console.log(results);
     objects = results;
+}
+function draw(){
+    if(status != "") {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i=0; i<objects.length; i++)
+        {
+
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects detected are: " + objects.length;
+            fill(r,g,b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+
 }
